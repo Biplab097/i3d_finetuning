68739be218f96c1ec3611ca9c2a66c2a2583b19e
Biplab Barman <biplabbarman097@gmail.com>
2021-06-07 23:29:27 +0530

    update train.py added STA

--- train/train.py
+++ train/train.py
@@ -25,26 +25,55 @@ import matplotlib.pyplot as plt
 from keras.optimizers import SGD # optimizer
 from keras.regularizers import l2
 from keras.layers import Dropout
+from keras.layers import Conv3D
+from keras.layers import Conv3DTranspose
 
 def train_i3d(X_train,Y_train,X_test,Y_test):
-    tf.keras.backend.set_floatx('float32')
+#     os.environ["CUDA_VISIBLE_DEVICES"]="1"
+    #tf.keras.backend.set_floatx('float16')
     i3d = "https://tfhub.dev/deepmind/i3d-kinetics-400/1"
 #     train_data = tf.data.Dataset.from_tensor_slices((X_train,Y_train))
 #     test_data = tf.data.Dataset.from_tensor_slices((X_test,Y_test))
+#     path = '/tf/i3d_finetuning/i3d-kinetics-400_1'
     hub_layer = hub.KerasLayer(i3d,input_shape=[None,224,224,3],trainable=False)
     #hub_layer(X_train[:2])
     model = tf.keras.Sequential()
     print("Here ------> ",type(model))
+    
+    model.add(Conv3D(8,kernel_size=(2,2,16),strides=(1, 1, 1),input_shape=(None,224,224,3),padding="same"))
+                            # o/p 224*224*3
+    model.add(Conv3DTranspose(8,kernel_size=(2,2,16),strides=(1, 1, 1),input_shape=(None,224,224,3),padding="same"))
+    model.add(tf.keras.layers.Dense(32,activation='relu'))
+    model.add(Conv3D(16,kernel_size=(2,2,16),strides=(1, 1, 1),input_shape=(None,224,224,3),padding="same"))
+    model.add(tf.keras.layers.Dense(3,activation='sigmoid'))
+    
+    
     model.add(hub_layer)
-    model.add(tf.keras.layers.Dense(256,activation='relu'))
-    model.add(Dropout(0.25))
+    
+    # Adding----------------------------------STA------------------------------
+    
+#     model.add(Conv3D(8,kernel_size=(2,2,16),strides=(1, 1, 1),input_shape=(224,224,3),padding="same"))
+#                             # o/p 224*224*3
+#     model.add(Conv3DTranspose(8,kernel_size=(2,2,16),strides=(1, 1, 1),input_shape=(224,224,3),padding="same"))
+#     model.add(tf.keras.layers.Dense(512,activation='relu'))
+#     model.add(Conv3D(16,kernel_size=(2,2,16),strides=(1, 1, 1),input_shape=(None,224,224,3),padding="same"))
+#     model.add(tf.keras.layers.Dense(256,activation='sigmoid'))
+    
+    
+    
+    #Added ----------------------------------- STA -------------------------------
+    
+    #model.add(tf.keras.layers.Dense(256,activation='relu')) #20-05-21
+    
+    #model.add(Dropout(0.25))
     # added dropout on 05-04-2021
-    model.add(tf.keras.layers.Dense(9, activation='softmax', kernel_regularizer=l2(0.001), bias_regularizer=l2(0.001)))
+    # model.add(tf.keras.layers.Dense(9, activation='softmax', kernel_regularizer=l2(0.001), bias_regularizer=l2(0.001)))
+    model.add(tf.keras.layers.Dense(6, activation='softmax')) # change as per no of classes 20-5-21
     # added kernel_regularizer on date 05-04-2021
 #     model.summary()
-    opt = SGD(learning_rate=(0.0002)) # 2*10^-4
-    model.compile(loss='categorical_crossentropy',optimizer=opt, metrics=['accuracy'])
-    print("here1------>",type(X_train))
+    opt = SGD(learning_rate=(0.002)) # 2*10^-3
+    model.compile(loss='categorical_crossentropy',optimizer=opt, metrics=['accuracy']) #20-5-21
+    #print("here1------>",type(X_train))
 #     X_train = tf.convert_to_tensor(X_train,dtype=tf.float32)
 #     Y_train = tf.convert_to_tensor(Y_train,dtype=tf.float32)
 #     X_test = tf.convert_to_tensor(X_test,dtype=tf.float32)
@@ -56,22 +85,9 @@ def train_i3d(X_train,Y_train,X_test,Y_test):
     
 #     history = model.fit(train_data,epochs=10)
     return model
-    # summarize history for accuracy
-#     plt.plot(history.history['accuracy'])
-#     plt.plot(history.history['val_accuracy'])
-#     plt.title('model accuracy')
-#     plt.ylabel('accuracy')
-#     plt.xlabel('epoch')
-#     plt.legend(['train', 'test'], loc='upper left')
-#     plt.show()
-#     # summarize history for loss
-#     plt.plot(history.history['loss'])
-#     plt.plot(history.history['val_loss'])
-#     plt.title('model loss')
-#     plt.ylabel('loss')
-#     plt.xlabel('epoch')
-#     plt.legend(['train', 'test'], loc='upper left')
-#     plt.show()
+  
+    # 07-06-2021
+
 
     
 
@@ -82,4 +98,4 @@ def train_i3d(X_train,Y_train,X_test,Y_test):
     
     
     
-    
+   
