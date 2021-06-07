dcbde003e80490dc5eeb7ac6e4dcb96b54122764
Biplab Barman <biplabbarman097@gmail.com>
2021-06-07 23:32:17 +0530

    updated preprocessor

--- lib/preprocessor.py
+++ lib/preprocessor.py
@@ -24,6 +24,9 @@ from IPython import display
 _VIDEO_LIST = None
 from urllib import request  # requires python3
 
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+T = 16 # depth of frames
+
 
 def crop_center_square(frame):
     y, x = frame.shape[0:2]
@@ -32,20 +35,65 @@ def crop_center_square(frame):
     start_y = (y // 2) - (min_dim // 2)
     return frame[start_y:start_y+min_dim,start_x:start_x+min_dim]
 
-def load_video(path, max_frames=0, resize=(224, 224)):
+def load_video(path,i,max_frames=0,resize=(224, 224)):
     cap = cv2.VideoCapture(path)
     frames = []
     try:
         while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            frame = crop_center_square(frame)
-            frame = cv2.resize(frame, resize)
-            frame = frame[:, :, [2, 1, 0]]
-            frames.append(frame)
+            if i==0 or i==10:
+                nframe = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+                #frames = [x * nframe / T for x in range(T)] // 4 frame - 64
+                #print("frames ",nframe)  # 3500
+                lis = []
+#                 diff = random.randint(0,nframe-65)  # 440
+#                 for i in range(16):
+#                     lis.append(diff) # [ 440,444,448,452,456....]
+#                     diff+=4 
+                
+                
+                for i in range(16):
+                    idx = random.randint(0,nframe) #4000 ->  (2,6,10,14....AP) 16s frames.
+                    lis.append(idx)
+                lis.sort() # [ 2,56,789,1004,2003 ]
+                #print("lis ",lis)
+#                 start = np.random.randint(0, nframe - (T+1))
+#                 end = start + T
+#                 print(start,end)
+#                 #frame = trim1(frame) 
+
+                for i in range(16):
+                    cap.set(cv2.CAP_PROP_POS_FRAMES, lis[i])
+                    ret, frame = cap.read()
+                    #print("frame shape before crop",frame.shape)
+                    if frame is not None:
+                        frame = crop_center_square(frame)
+                        frame = cv2.resize(frame, resize)
+                        frame = frame[:, :, [2, 1, 0]]
+                        #print("shape of frame ",frame.shape)
+                        frames.append(frame)
+#                 for i in range(start,end):
+#                     cap.set(cv2.CAP_PROP_POS_FRAMES, frames[i])
+#                     ret, frame = cap.read()
+#                     frame = crop_center_square(frame)
+#                     frame = cv2.resize(frame, resize)
+#                     frame = frame[:, :, [2, 1, 0]]
+#                     print("shape of frame ",frame.shape)
+#                     frames.append(frame)
+                    
+            else:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                frame = crop_center_square(frame)
+                frame = cv2.resize(frame, resize)
+                frame = frame[:, :, [2, 1, 0]]
+            
+                #print("appending frame ",frame.shape)
+                frames.append(frame)
 
             if len(frames) == max_frames:
+                if(i==0):
+                    print("frame size 0") 
                 break
     finally:
         cap.release()
@@ -59,31 +107,52 @@ def loaddata(Video_dir,n_classes):
     for i in range(n_classes):
         path = os.path.join(Video_dir, 'c'+str(i),'*.avi')
         files = glob.glob(path)
-        print("load data entry point")
-      
+        print("load data entry point ",i+1)
+        no_of_files = 0
         for filename in files:
             labels.append(i)
             #print("in load data count->",count)
-            X.append(load_video(filename))
-
+            X.append(load_video(filename,i))
+            if(i==0 or i==10):
+                no_of_files+=1
+                print("no of files added ",no_of_files)
+    print("loading done")
     return np.array(X) , np.array(labels)
 
-T = 16
-
-def trim(video):
+def trim1(video):
     start = np.random.randint(0, video.shape[0] - (T+1)) #changed shape[1] to shape[0]
     end = start + T
-    print("Start and end ",start,end)
-    return np.array(video[start:end, :, :, :])
+    #print("Start and end ",start,end)
+    return np.array(video[start:end, :, :])
 
+def trim(video,i):
+    #print("video.shape[0] ",video.shape[0])
+    try:
+        start = np.random.randint(0, video.shape[0] - (T+1)) #changed shape[1] to shape[0]
+        end = start + T
+#         print("======")
+#         print("=============")
+#         print("==========================")
+        #print("Start and end ",start,end)
+        return np.array(video[start:end, :, :, :])
+    except:
+        print("video.shape[0] ",video.shape[0],i)
+        return np.random.rand(16,224,224,3)
+    
 
+T = 16
 def train_array(X_train):
     X = []
+    i = 0
     for ele in X_train:
 #         print("shape",ele.shape)
-        ele = trim(ele)
+        i+=1
+        if ele is not None:
+            ele = trim(ele,i)
+        else:
+            ele =  np.random.rand(16,224,224,3)
         X.append(ele)
-        print("shape",ele.shape)
+        #print("shape",ele.shape)
     print("from train array",len(X))
     
     return np.array(X)
